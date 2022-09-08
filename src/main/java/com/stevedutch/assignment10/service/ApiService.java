e57aea283c9bf8ad584a0a0bfac486fddb549e86
SteveDutch <steholl@posteo.org>
2022-09-08 23:50:02 +0200

    fix null handling at GetMappings via Optional

--- src/main/java/com/stevedutch/assignment10/service/ApiService.java
+++ src/main/java/com/stevedutch/assignment10/service/ApiService.java
@@ -1,12 +1,13 @@
 package com.stevedutch.assignment10.service;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
+  
 @Component
 public class ApiService {
 
@@ -14,6 +15,7 @@ public class ApiService {
 	private String keyValue;
 	RestTemplate rt = new RestTemplate();
 	
+	// TODO wenn keine if-Bedingungsprüfung, brauche ich diese Methode auch nicht mehr
 	public URI callDefault(String timeFrame) {
 		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
 				.queryParam("apiKey", keyValue)
@@ -26,9 +28,14 @@ public class ApiService {
 	public URI callApi(Integer numCalories, String diet, String exclusions, String timeFrame) {
 		// TODO check: maxCalories & targetCalories at spoonacular
 		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
-				.queryParam("apiKey", keyValue).queryParam("timeFrame", timeFrame)
-				.queryParam("targetCalories", numCalories).queryParam("diet", diet)
-				.queryParam("intolerances", exclusions).build().toUri();
+				.queryParam("apiKey", keyValue)
+				.queryParam("timeFrame", timeFrame)
+				.queryParamIfPresent("targetCalories", Optional.ofNullable(numCalories))
+				.queryParamIfPresent("diet", Optional.ofNullable(diet))
+				//.queryParam("targetCalories", numCalories).queryParam("diet", diet)
+				.queryParamIfPresent("intolerances", Optional.ofNullable(exclusions))
+				//.queryParam("intolerances", exclusions)
+				.build().toUri();
 		return uri;
 	}
 
--- src/main/java/com/stevedutch/assignment10/web/WebController.java
+++ src/main/java/com/stevedutch/assignment10/web/WebController.java
@@ -1,5 +1,7 @@
 package com.stevedutch.assignment10.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +24,14 @@ public class WebController {
 	@GetMapping("mealplanner/day")
 	public ResponseEntity<DayResponse> getDayMeals(String numCalories, String diet, String exclusions) {
 		String timeFrame = "day";
-		if (numCalories == null || diet == null || exclusions == null) {
-			ResponseEntity<DayResponse> response = rt.getForEntity(apiService.callDefault(timeFrame), DayResponse.class);
-			return response;
-		}
-		Integer maxCal = Integer.valueOf(numCalories);
+		// funktioniert nicht,weil bereits 1 fehlender Wert reicht, um callDefault() aufzurufen
+//		if (numCalories == null || diet == null || exclusions == null) {
+//			ResponseEntity<DayResponse> response = rt.getForEntity(apiService.callDefault(timeFrame), DayResponse.class);
+//			return response;
+//		}
+		// Integer maxCal = Integer.valueOf(numCalories); Problem: what if = null? stattdessen Versuch lt. 
+		// https://stackoverflow.com/questions/51838827/convert-a-string-representation-of-number-to-integer-in-java-8single-line-with
+		Integer maxCal = Optional.ofNullable(numCalories).map(Integer::valueOf).orElse(null);
 		ResponseEntity<DayResponse> response = rt.getForEntity(apiService.callApi(maxCal, diet, exclusions, timeFrame),
 				DayResponse.class);
 		return response;
@@ -37,14 +42,25 @@ public class WebController {
 		String timeFrame = "week";
 		// TODO check here! Roches Bug entspricht: 500 / null / null 
 		// maybe try optionals ?
-		if (numCalories == null || diet == null || exclusions == null) {
-			ResponseEntity<WeekResponse> response = rt.getForEntity(apiService.callDefault(timeFrame), WeekResponse.class);
-			return response;
-		}
-		Integer maxCal = Integer.valueOf(numCalories);
+//		if (numCalories == null || diet == null || exclusions == null) {
+//			ResponseEntity<WeekResponse> response = rt.getForEntity(apiService.callDefault(timeFrame), WeekResponse.class);
+//			return response;
+//		}
+		Integer maxCal = numCaloriesNullCheck(numCalories);
+//		Optional<String> opt = Optional.of(numCalories); --> numCalories darf nicht null sein, sonst NullPointeerExceütion TODO Lernertrag
+
+	
 		ResponseEntity<WeekResponse> weekMeal = rt.getForEntity(apiService.callApi(maxCal, diet, exclusions, timeFrame),
 				WeekResponse.class);
 		return weekMeal;
+	}
+
+	public Integer numCaloriesNullCheck(String numCalories) {
+		Integer maxCal;
+		if (numCalories != null) {
+			maxCal = Integer.valueOf(numCalories);
+		} else { maxCal = 1800;}
+		return maxCal;
 	}
 
 }
